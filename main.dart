ece01cb41bd4a6c00cafc9867f4a2ea2b6c9dbe6
Grace <grace6424@icloud.com>
2025-02-18 12:30:49 +0100

    123

---
+++ main.dart
@@ -0,0 +1,32 @@
+void main() {
+
+  int d = 20;
+  int seven = 7;
+  int twelve = 12;
+  int a = 24;
+  String dgStr = "Guten";
+  String dayStr = "Tag";
+  String worldStr = "Welt";
+  String timeStr = "Zeit";
+  String D = "!";
+  String c = "Schöne";
+  String morning = "Morgen";
+  String S = "Heute";
+  String spa = " ";
+
+  String goodDay = "$dgStr $dayStr";
+  String goodDayWorld = "$dgStr $dayStr $worldStr$D";
+  int thirtyOne = a + seven;
+  String goodTime = "$c $timeStr$D";
+  String thisMorning = "$S $morning$D";
+  int addUp = d + seven + twelve + a + 105;
+
+print(goodDay);
+print(goodDayWorld);
+print(thirtyOne);
+print(goodTime);
+print(thisMorning);
+print(addUp);
+print("sorry, but you just lost the game$spa$D");
+
+}
